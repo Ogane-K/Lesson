368e865ec791c8e00b9d1d4f8441219c8fbbd713
Ogane-K <syokuensan@gmail.com>
2025-06-05 21:59:40 +0900

    会員用の予約一覧ページ/テスト済

--- src/main/java/com/example/samuraitravel/entity/House.java
+++ src/main/java/com/example/samuraitravel/entity/House.java
@@ -1,19 +1,24 @@
 package com.example.samuraitravel.entity;
 
 import java.sql.Timestamp;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @Entity
 @Table(name = "houses")
+@ToString(exclude = "reservations")
 public class House {
 
 	@Id
@@ -50,5 +55,8 @@ public class House {
 
 	@Column(name = "updated_at", insertable = false, updatable = false)
 	private Timestamp updatedAt;
+
+	@OneToMany(mappedBy = "house" , fetch = FetchType.EAGER, cascade =  CascadeType.REMOVE)
+	private List<Reservation> reservations;
 
 }
---
+++ src/main/java/com/example/samuraitravel/service/ReservationService.java
@@ -0,0 +1,26 @@
+package com.example.samuraitravel.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.example.samuraitravel.entity.Reservation;
+import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.repository.ReservationRepository;
+
+@Service
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository){
+        this.reservationRepository = reservationRepository;
+    }
+
+    // 指定されたユーザーに紐づく予約を作品日時が新しい順に並び替え、ページングされた状態で取得
+    public Page<Reservation> findReservationsByUserOrderByCreatedAtDesc(User user,Pageable pageable){
+        return reservationRepository.findByUserOrderByCreatedAtDesc(user, pageable);
+    }
+    
+
+}
